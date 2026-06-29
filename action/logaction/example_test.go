5805b0548a05f71a402bef4c7f4da5f40cadccae
zakyalvan <zakyalvan@gmail.com>
2026-06-29 11:40:21 +0700

    feat(action/logaction): structured-log pass-through action

---
+++ action/logaction/example_test.go
@@ -0,0 +1,23 @@
+package logaction_test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/zakyalvan/krtlwrkflw/action/logaction"
+)
+
+func ExampleNewLog() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+	}))
+	a := logaction.NewLog(logaction.WithLogger(logger), logaction.WithMessage("audit"), logaction.WithKeys("user"))
+	_, _ = a.Do(context.Background(), map[string]any{"user": "ada", "secret": "x"})
+	// Output: level=INFO msg=audit user=ada
+}
